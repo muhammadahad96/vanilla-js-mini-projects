d84ac22d620638ccb11986185beaec26c07283da
Muhammad Ahad <muhammadahad9615@gmail.com>
2020-05-25 23:54:03 +0500

    added exchange rate project

---
+++ Exchange Rate/script.js
@@ -0,0 +1,41 @@
+/* eslint-disable camelcase */
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function calculateRate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      const rate = data.rates[currency_two];
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculateRate);
+amountEl_one.addEventListener('input', calculateRate);
+currencyEl_two.addEventListener('change', calculateRate);
+amountEl_two.addEventListener('input', calculateRate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculateRate();
+});
+
+// function call
+calculateRate();
